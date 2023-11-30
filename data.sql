c552faee4c2e08bd5948445d69f5d67c39138664
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-11-29 22:00:14 -0300

    Começo da Inserção dos Dados

---
+++ data.sql
@@ -0,0 +1,9 @@
+USE Floodguard;
+
+INSERT INTO tblUf VALUES ('SP', 'São Paulo');
+
+INSERT INTO tblUf VALUES ('RJ', 'Rio de Janeiro');
+
+INSERT INTO tblUf VALUES ('SC', 'Santa Catarina');
+
+INSERT INTO tblCidade VALUES ('PE', '');
